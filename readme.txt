dc3a8bfc85301c11e2a7da99e564816abc83ce73
mbsec <mbsec@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-16 12:30:16 +0000

    new version 1.0.0-beta.2 with improved user interface
    git-svn-id: https://plugins.svn.wordpress.org/mb-sitemap-generator/trunk@1161782 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -3,14 +3,14 @@ Contributors: mbsec
 Tags: sitemap, site map
 Requires at least: 4.2
 Tested up to: 4.2.2
-Stable tag: 1.0.0-beta.1
+Stable tag: 1.0.0-beta.2
 License: AGPL v3
 License URI: https://www.gnu.org/licenses/agpl-3.0.html
 
-An easy to use sitemap generator for WordPress.
+An easy to use XML sitemap generator for WordPress.
 
 == Description ==
-The [sitemap generator](https://www.marcobeierer.com/tools/sitemap-generator#wordpress) uses an external service to crawl your website and create a sitemap of your website. The generator works thus for every plugin out of the box. The computation costs for your website is also very low because the crawler acts like a normal visitor, who visits all pages once.
+The [sitemap generator](https://www.marcobeierer.com/tools/sitemap-generator#wordpress) uses an external service to crawl your website and create a XML sitemap of your website. The generator works thus for every plugin out of the box. The computation costs for your website is also very low because the crawler acts like a normal visitor, who visits all pages once.
 
 = Features =
 * Simple setup.
@@ -29,12 +29,17 @@ If you already have an existing sitemap.xml in your WordPress root directory, th
 == Installation ==
 1. Upload the 'mb-sitemap-generator' folder to the '/wp-content/plugins/' directory.
 2. Activate the plugin through the 'Plugins' menu in WordPress.
-3. Use the "Generate Sitemap" button the create a sitemap. Be aware that the page loads and is not shown until the generation of the sitemap is done. The sitemap will be saved as sitemap.xml in your WordPress root directory. **Be aware that an existing sitemap.xml file would be overwritten without asking.**
-4. Access http(s)://www.example.com/sitemap.xml and check if the generated sitemap is complete.
+3. Access the generator with the "Sitemap Generator" button in the sidebar and use the "Generate your sitemap" button to start the generation process. The sitemap will be saved as sitemap.xml in your WordPress root directory. **Be aware that an existing sitemap.xml file would be overwritten without asking.**
+4. Use the "Show the sitemap" button to download your sitemap and check if the generated sitemap is complete.
 
 == Changelog ==
 
 = 1.0.0-beta.1 =
 *Release Date - 9th May, 2015*
 
-* Initial release
+* Initial release.
+
+= 1.0.0-beta.2 =
+*Release Date - 16th May, 2015*
+
+* Improved the user interface.
--- sitemapgenerator.php
+++ sitemapgenerator.php
@@ -9,7 +9,7 @@
 Plugin Name: Sitemap Generator
 Plugin URI: https://www.marcobeierer.com/tools/sitemap-generator#wordpress
 Description: A easy to use Sitemap Generator for WordPress
-Version: 1.0.0-beta.1
+Version: 1.0.0-beta.2
 Author: Marco Beierer
 Author URI: https://www.marcobeierer.com
 License: AGPL
@@ -17,12 +17,84 @@ Text Domain: Marco Beierer
 */
 
 add_action('admin_menu', 'register_sitemap_generator_page');
-
-function register_sitemap_generator_page(){
-	add_menu_page( 'Generate Sitemap', 'Generate Sitemap', 'manage_options', 'sitemap-generator', 'sitemap_generator_page', '', 99); 
+function register_sitemap_generator_page() {
+	add_menu_page('Sitemap Generator', 'Sitemap Generator', 'manage_options', 'sitemap-generator', 'sitemap_generator_page', '', 99); 
 }
 
-function sitemap_generator_page(){
+function sitemap_generator_page() {
+?>
+	<h2>Sitemap Generator</h2>
+	<div class="card" id="sitemap-widget" ng-app="sitemapGeneratorApp" ng-strict-di>
+		<h3>Generate a XML sitemap of your site</h3>
+        <div ng-controller="SitemapController">
+            <form name="sitemapForm">
+                <div class="input-group">
+                    <span class="input-group-addon">
+                        <i class="glyphicon glyphicon-globe"></i>
+                    </span>
+                    <span class="input-group-btn">
+                        <button type="submit" class="button {{ generateClass }}" ng-click="generate()" ng-disabled="generateDisabled">Generate your sitemap</button>
+                        <a class="button {{ downloadClass }}" ng-click="download()" ng-disabled="downloadDisabled" download="sitemap.xml" ng-href="{{ href }}">Show the sitemap</a>
+                    </span>
+                </div>
+            </form>
+            <p class="alert well-sm {{ messageClass }}">{{ message }} <span ng-if="pageCount > 0 && downloadDisabled">{{ pageCount }} pages already crawled.</span></p>
+        </div>
+    </div>
+	<script defer src="<?php echo get_site_url(); ?>/wp-content/plugins/mb-sitemap-generator/js/angular.min.js"></script>
+    <script defer src="<?php echo get_site_url(); ?>/wp-content/plugins/mb-sitemap-generator/js/sitemap.js?v=1"></script>
+<?
+}
+
+add_action('wp_ajax_sitemap_proxy', 'sitemap_proxy_callback');
+function sitemap_proxy_callback() {
+
+	$baseurl = get_site_url();
+	$baseurl64 = strtr(base64_encode($baseurl), '+/', '-_');
+
+	$ch = curl_init();
+
+	curl_setopt($ch, CURLOPT_URL, 'https://api.marcobeierer.com/sitemap/v2/' . $baseurl64 . '?pdfs=1&origin_system=wordpress');
+	curl_setopt($ch, CURLOPT_HEADER, 0);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+	$response = curl_exec($ch);
+
+	$statusCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+	$contentType = curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
+
+	curl_close($ch);
+
+	if ($statusCode == 200 && $contentType == 'application/xml') {
+
+		$reader = new XMLReader();
+		$reader->xml($response, 'UTF-8');
+		$reader->setParserProperty(XMLReader::VALIDATE, true);
+
+		if ($reader->isValid()) { // TODO check if empty?
+
+			$rootPath = get_home_path();
+			if ($rootPath != '') {
+				file_put_contents($rootPath . DIRECTORY_SEPARATOR . 'sitemap.xml', $response); // TODO handle and report error
+			}
+		}
+	}
+
+	if (function_exists('http_response_code')) {
+		http_response_code($statusCode);
+	}
+	else { // fix for PHP version older than 5.4.0
+		$protocol = (isset($_SERVER['SERVER_PROTOCOL']) ? $_SERVER['SERVER_PROTOCOL'] : 'HTTP/1.0');
+		header($protocol . ' ' . $statusCode . ' ');
+	}
+
+	header("Content-Type: $contentType");
+
+	echo $response;
+	wp_die();
+}
+
+/*function sitemap_generator_page(){
 
 	$PLG_AJAX_SITEMAPGENERATOR_SUCCESS="The generation of the sitemap was successfull. The sitemap was saved as sitemap.xml in the WordPress root folder.";
 	$PLG_AJAX_SITEMAPGENERATOR_ERROR="An error occurred. Please try it again or contact the developer of the extension.";
@@ -75,4 +147,5 @@ function sitemap_generator_page(){
 
 	echo $PLG_AJAX_SITEMAPGENERATOR_ERROR; // TODO better error message
 	return;
-}
+}*/
+
